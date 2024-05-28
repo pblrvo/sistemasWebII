b8485bf21341af7205f22c556ea6a2b81524d2d8
Daguerre45 <alberto.daguerre02@gmail.com>
2024-05-28 19:45:06 +0200

    Añadido de cositas

--- api/app.js
+++ api/app.js
@@ -1,17 +1,17 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const peliculasRouter = require('./routes/peliculas');
 
-var app = express();
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(process.env.BASE_URI + '/peliculas', peliculasRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
