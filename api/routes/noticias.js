3269763acbd1fce8b5510ac64f30a9029478b77f
juaki0315 <109316446+juaki0315@users.noreply.github.com>
2024-06-06 20:19:11 +0200

    hecho lo del xml

--- api/routes/noticias.js
+++ api/routes/noticias.js
@@ -1,20 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const parser = require('xml2json');
 const axios = require('axios');
 
 router.get('/', async (req, res) => {
     try {
-      const count = req.query.count || 3;
-      const currentOffset = Math.floor(Date.now() / 1000); // Obtener la fecha y hora actual en segundos
-      const response = await axios.get(`http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=440&count=${count}&maxlength=300&format=xml&enddate=${currentOffset}`);
-      const jsonData = parser.toJson(response.data, { object: true });
-      res.json({ data: jsonData, offset: currentOffset });
+        const count = req.query.count || 3;
+        const response = await axios.get(`http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=440&count=${count}&maxlength=300&format=xml`);
+        
+        res.set('Content-Type', 'application/xml');
+        res.send(response.data);
     } catch (error) {
-      console.error('Error al obtener los datos de la API externa:', error);
-      res.status(500).send('Error al obtener los datos de la API externa');
+        console.error('Error al obtener los datos de la API externa:', error);
+        res.status(500).send('Error al obtener los datos de la API externa');
     }
-  });
-
+});
 
 module.exports = router;
--- cliente/routes/noticias.js
+++ cliente/routes/noticias.js
@@ -5,13 +5,15 @@ const axios = require('axios');
 
 router.get('/', async (req, res) => {
     try {
-      const response = await axios.get('http://localhost:3010/api/v1/noticias');
-      res.render('noticias', { data: response.data.data, offset: response.offset });
-    } catch (error) {
-      console.error('Error al obtener los datos de la API externa:', error);
-      res.status(500).send('Error al obtener los datos de la API externa');
-    }
-  });
+        const response = await axios.get('http://localhost:3010/api/v1/noticias', { headers: { 'Accept': 'application/xml' } });
+        
+        const jsonData = parser.toJson(response.data, { object: true });
 
+        res.render('noticias', { data: jsonData });
+    } catch (error) {
+        console.error('Error al obtener los datos de la API externa:', error);
+        res.status(500).send('Error al obtener los datos de la API externa');
+    }
+});
 
 module.exports = router;
